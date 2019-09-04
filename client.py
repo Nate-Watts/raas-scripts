bf28a564669889c035384f823fe384eacee9a62a
MangoVerde-Con-Sal <nwatts@saltstack.com>
2019-09-04 14:11:35 -0600

    response

--- client.py
+++ client.py
@@ -3,3 +3,4 @@ from sseapiclient.tornado import SyncClient
 raas = 'http://localhost:8080'
 client = SyncClient.connect(raas, 'root', 'salt', rpc_api_version='2', force_restfull=False)
 response = client.api.job.get_jobs(page=1).ret
+print(response)
